6384705f9d99c3b189bb948fb97c17447c5bfc9c
ugly-kid-af <dwarmachine24@gmail.com>
2020-06-18 08:12:22 +0000

    Revert "violet: Import init.recovery.qcom"
    This reverts commit 0b76e1933decf8d6e0edb3882cd5e6bd867929c2.

--- device.mk
+++ device.mk
@@ -342,7 +342,6 @@ PRODUCT_PACKAGES += \
     init.qcom.early_boot.sh \
     init.qcom.post_boot.sh \
     init.qcom.rc \
-    init.recovery.qcom.rc \
     init.qcom.sensors.sh \
     init.qti.chg_policy.sh \
     init.qcom.sh \
